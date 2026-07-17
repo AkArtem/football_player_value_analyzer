fa6ac7070dc36566e8f91a208d46586680128ddc
AkArtem <akulichartem2@gmail.com>
2026-07-17 19:07:57 +0200

    Add SQL queries for top scorers and stats per 90 minutes analysis

---
+++ sql/stats_per_90.sql
@@ -0,0 +1,7 @@
+SELECT name, total_goals, total_assists, total_minutes,
+ROUND(total_goals/total_minutes*90, 3) AS goals_per_90, 
+ROUND(total_assists/total_minutes*90, 3) AS assists_per_90
+FROM players_fresh_stats
+WHERE total_minutes >= 5000
+ORDER BY goals_per_90 DESC 
+LIMIT 10;
---
+++ sql/top_scorers.sql
@@ -0,0 +1,4 @@
+SELECT p.player_id, p.name, s.total_goals, s.total_appearances
+FROM players_fresh_stats AS s JOIN players AS p ON s.player_id=p.player_id
+ORDER BY s.total_goals DESC
+LIMIT 10;
