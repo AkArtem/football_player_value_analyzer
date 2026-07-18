7b764f11910dcff22b7f80a72d2d5f4060fa700c
AkArtem <akulichartem2@gmail.com>
2026-07-18 17:28:05 +0200

    Fix value change calculation in SQL query

--- sql/value_change.sql
+++ sql/value_change.sql
@@ -1,6 +1,8 @@
 SELECT v.date, p.name, v.market_value_in_eur,
-v.market_value_in_eur - LAG(v.market_value_in_eur, 1, 0)
-OVER(PARTITION BY p.name ORDER BY v.date) AS value_change
+v.market_value_in_eur - COALESCE(
+    LAG(v.market_value_in_eur) OVER(PARTITION BY p.name ORDER BY v.date),
+    v.market_value_in_eur
+) AS value_change
 FROM player_valuations AS v JOIN players AS p ON v.player_id=p.player_id
 WHERE v.player_id = 8198
 ORDER BY v.date
