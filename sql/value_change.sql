ceb3cbb13f2e9ff0df6480131af4b35147a76e65
AkArtem <akulichartem2@gmail.com>
2026-07-21 13:19:20 +0200

    Fix value change calculation by updating partitioning to use player_id instead of name

--- sql/value_change.sql
+++ sql/value_change.sql
@@ -1,6 +1,6 @@
 SELECT v.date, p.name, v.market_value_in_eur,
 v.market_value_in_eur - COALESCE(
-    LAG(v.market_value_in_eur) OVER(PARTITION BY p.name ORDER BY v.date),
+    LAG(v.market_value_in_eur) OVER(PARTITION BY p.player_id ORDER BY v.date),
     v.market_value_in_eur
 ) AS value_change
 FROM player_valuations AS v JOIN players AS p ON v.player_id=p.player_id
